4895451fc9b4c77c363ad5bb00a2176b53f7592a
Iyad Shobaki <iyadshobaki82@gmail.com>
2024-04-24 15:07:44 -0400

    Add project files.

---
+++ WPFMiniProject/AddressEntry.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System.Windows;
+
+namespace WPFMiniProject
+{
+   /// <summary>
+   /// Interaction logic for AddressEntry.xaml
+   /// </summary>
+   public partial class AddressEntry : Window
+   {
+      // Who ever call this class is implement ISaveAddress and will get 
+      // address info adding to the list
+      ISaveAddress _parent;
+      public AddressEntry(ISaveAddress parent)
+      {
+         InitializeComponent();
+
+         _parent = parent;
+      }
+
+      private void saveAddressButton_Click(object sender, RoutedEventArgs e)
+      {
+         AddressModel address = new AddressModel
+         {
+            StreetAddress = streetAddressText.Text,
+            City = cityText.Text,
+            State = stateText.Text,
+            ZipCode = zipCodeText.Text
+         };
+
+         _parent.SaveAddress(address);
+
+         this.Close();
+      }
+   }
+}
---
+++ WPFMiniProject/PersonEntry.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DemoLibrary;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace WPFMiniProject
+{
+   /// <summary>
+   /// Interaction logic for PersonEntry.xaml
+   /// </summary>
+   public partial class PersonEntry : Window, ISaveAddress
+   {
+      BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+
+
+      public PersonEntry()
+      {
+         InitializeComponent();
+
+         addressesListBox.ItemsSource = addresses;
+
+         //addresses.Add(new AddressModel
+         //{
+         //   StreetAddress = "123 Wallabee Way",
+         //   City = "Clarks Summit",
+         //   State = "PA",
+         //   ZipCode = "18411"
+         //});
+      }
+
+      public void SaveAddress(AddressModel address)
+      {
+         addresses.Add(address);
+      }
+
+      private void addAddressButton_Click(object sender, RoutedEventArgs e)
+      {
+         AddressEntry entry = new AddressEntry(this);
+
+         entry.Show();
+
+      }
+
+      private void savePersonButton_Click(object sender, RoutedEventArgs e)
+      {
+         PersonModel person = new PersonModel
+         {
+            FirstName = firstNameText.Text,
+            LastName = lastNameText.Text,
+            IsActive = (activeCheckBox.IsChecked ?? false),
+            // the 2 question marks check if its null will put false value
+            Addresses = addresses.ToList()
+         };
+
+
+      }
+   }
+}
